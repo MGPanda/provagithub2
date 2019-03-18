beab4c04ac1684c7c755826adaf43693b1a96a41
Miquel Alburquerque Marín <miquelabur@gmail.com>
2019-03-18 16:56:34 +0100

    Prova

--- examen2tri/src/control/Menu.java
+++ examen2tri/src/control/Menu.java
@@ -14,7 +14,7 @@ public class Menu {
 	private AgenciaViatges av = new AgenciaViatges();
 	
 	public void start() {
-		System.out.println("Programa per Tòfol Martínez Lorenzo.");
+		System.out.println("Programa per Tòfol Martínez Lorenzo. Modificat per Miquel Alburquerque");
 		creacioDades();
 		int opt=1;
 		while(opt!=4) {
